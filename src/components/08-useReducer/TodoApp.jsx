06ee19506f4b6db892bc10960ddde4d1240143d0
Simon <simonbustamante21@gmail.com>
2023-03-23 21:27:31 -0300

    fin de la sección del useReducer (manejo de estados en memoria)

--- src/components/08-useReducer/TodoApp.jsx
+++ src/components/08-useReducer/TodoApp.jsx
@@ -1,65 +1,22 @@
-import { useEffect, useReducer } from "react"
+
 import { TodoAdd } from "./TodoAdd";
 import { TodoList } from "./TodoList";
-import { todoReducer } from "./todoReducer"
-
-const initialState = [
-    // {
-    //     id: new Date().getTime(),
-    //     description: 'Recolectar la piedra del Alma',
-    //     done: false,
-    // },
-    // {
-    //     id: new Date().getTime() * 3,
-    //     description: 'Recolectar la piedra del Tiempo',
-    //     done: false,
-    // }
-]
-
-const init = () => {
-    return JSON.parse(localStorage.getItem('todos')) || [];
-};
+import { useTodo } from '../../hooks/useTodo'
 
 export const TodoApp = () => {
 
-
-    const [state, dispatch] = useReducer(todoReducer, initialState, init);
-
-    useEffect(() => {
-        localStorage.setItem('todos', JSON.stringify(state));
-    }, [state])
-    
-
-    const handleNewTodo = (todo) => {
-        const action = {
-            type: '[TODO] add todo',
-            payload: todo
-        }
-        dispatch(action);
-    }
-    
-    const handleRemoveTodo = (id) => {
-        const action = {
-            type: '[TODO] remove todo',
-            payload: id
-        }
-        dispatch(action);
-         
-    };
-
-    const handleToggleTodo = (id) => {
-        const action = {
-            type: '[TODO] toggle todo',
-            payload: id
-        }
-
-        dispatch(action);
-         
-    };
+    const {
+        state,
+        todosCount,
+        pendingTodosCount,
+        handleNewTodo,
+        handleRemoveTodo,
+        handleToggleTodo
+    } = useTodo();
 
     return (
         <>
-            <h1>Todo: 10, <small>pendientes: 1</small></h1>
+            <h1>Todo: {todosCount}, <small>pendientes: {pendingTodosCount} </small></h1>
             <hr />
 
             <div className="row">
